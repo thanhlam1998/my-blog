0ed9e5beef81e00519acea14f5673156dff4bb6b
thanhlam <lam.nguyen@pascal.studio>
2021-11-08 22:56:02 +0700

    add graphql query to the next service

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import 'tailwindcss/tailwind.css'
+import React from "react";
+import { Layout } from "../components/";
+
+import "tailwindcss/tailwind.css";
+import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />;
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
